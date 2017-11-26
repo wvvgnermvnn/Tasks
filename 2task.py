0b39b6ee51032213daa3aa48518b05a80f0216d5
phil <fvagner97@mail.ru>
2017-11-27 01:15:32 +0300

    second task added

---
+++ 2task.py
@@ -0,0 +1,17 @@
+
+def chunks(lst, chunk_count):
+    chunk_size = len(lst) // chunk_count
+    return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
+
+if __name__ == '__main__':
+    N = int(input("Введите размер N: "))
+    vector = list(i + 1 for i in range(N))
+    while True:
+        M = int(input("Введите число частей M (М должно быть меньше N): "))
+        if M < N and len(chunks(vector, M)) == M:
+            break
+        else:
+            print()
+            print("Вы ввели число M, не меньшее числа N, \nлибо вектор невозможно разделить на введённое Вами число М")
+    print("Вектор = ", vector)
+    print('Разделение вектора: ',chunks(vector, M))
