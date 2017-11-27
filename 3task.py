6c95d8d4848b60e71c0e28bd47442f32c1e2bb4b
phil <fvagner97@mail.ru>
2017-11-27 04:14:54 +0300

    Holla!

--- 3task.py
+++ 3task.py
@@ -3,48 +3,25 @@
 # data = pd.read_csv('mlcourse_open/data/telecom_churn.csv')
 # print(data.describe())
 
+"""Usage: parse-args --number=<int> [--flag]"""
+import inspect
+import functools
 import sys
 
-PARAMS = {'--integer': True,'--boolean': True,'--string': False}
-
-def param_value_or_none(param_name):
-    for arg in sys.argv:
-        if param_name in arg:
-            return arg.split('=')[1]
-
-    return None
-
-
-def generate_help_message(file_name):
-    msg = ''
-    for param_name, required in PARAMS.items():
-        if not required:
-            param_name = '[{}] '.format(param_name)
-        msg += param_name + ' '
-    msg = 'Use: {fname} {rest_msg}'.format(fname=file_name, rest_msg=msg)
-    return msg
-
-class GetOpt:
-    def getopt():
-        lst = []
-        fh = sys.argv[0]
-
-        if len(sys.argv) == 1 or True in ['help' in arg for arg in sys.argv]:
-            return generate_help_message(fh)
-
-        for param_name, required in PARAMS.items():
-            value = param_value_or_none(param_name)
-
-            if required:
-                if value is None:
-                    # returns warning about required value
-                    return ' '.join([param_name, 'required'])
-                lst.append({param_name: value})
-            else:
-                lst.append({param_name: value})
-
-        return lst
-
-
-if __name__ == '__main__':
-    print(GetOpt.getopt())
+def simple_cli(description):
+    """Implement a simple annotation-based command-line parser."""
+    def decorator(main):
+        @functools.wraps(main)
+        def wrapper(argv=None):
+            if argv is None:
+                argv = sys.argv
+            if '--help' in argv:
+                sys.exit(description)
+            params = inspect.signature(main).parameters
+            parser = ArgumentParser.from_params(*params.values())
+            try:
+                return main(**parser.parse_args(argv[1:]))
+            except ArgumentError as e:
+                sys.exit(f"Error: {e}\n{description}")
+        return wrapper
+    return decorator
